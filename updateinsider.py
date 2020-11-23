5297c30308d9a8e1f232117b34cba6be3d80e5bb
Raspiduino <raspiduinogit@gmail.com>
2020-11-23 22:20:25 +0700

    New Commit

--- updateinsider.py
+++ updateinsider.py
@@ -59,12 +59,12 @@ open("scripts\\tmp\\uup_download_windows.tmp.cmd", "w").write(open("scripts\\uup
 # Run the uupdump script
 os.system("scripts\\tmp\\uup_download_windows.tmp.cmd") # This might take a while. Go and have a cup of coffee :D
 
-# Mount vhd file
+# Mount vhd file and format it
 open("scripts\\tmp\\mountvhd.tmp.txt", "w").write(open("scripts\\mountvhd.txt", "r").read().replace("file=", "file=" + vhd).replace("letter=", "letter=" + driveletter))
 os.system('diskpart /s scripts\\tmp\\mountvhd.tmp.txt')
 
 # Mount iso
-isodrivename = os.system('start /wait powershell "exit [int][char](Mount-DiskImage -PassThru ' + [f for f in os.listdir('scripts\\tmp') if os.path.isfile(os.path.join('.', f)) and f.endswith('.iso')][0] + ' | Get-Volume).DriveLetter;"')
+isodrivename = os.system('powershell "exit [int][char](Mount-DiskImage -PassThru "' + os.getcwd() + '\\scripts\\tmp\\' + [f for f in os.listdir('scripts\\tmp\\') if f.endswith('.ISO')][0] + '" | Get-Volume).DriveLetter;"')
 
 # Extract the wim file
 os.system("scripts\\files\\wimlib-imagex apply " + chr(isodrivename) + ":\\sources\\install.wim " + driveletter + ":\\") # This might take a while too!
